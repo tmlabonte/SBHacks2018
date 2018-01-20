63762eaa2f4d36b8bec316802ec5cf0954352471
Tyler LaBonte <tmlabonte@gmail.com>
2018-01-20 09:20:29 -0800

    Fixed href bug:

--- search.js
+++ search.js
@@ -25,23 +25,19 @@ function onSearchResponse(response) {
 // store url on load
 var rootPage = "https://tmlabonte.github.io/SBHacks2018/";
 
-// listen for changes
-setInterval(function()
+if (window.location.href != rootPage)
 {
-    if (window.location.href != rootPage)
-    {
-    	var cities = ["Los Angeles", "New York", "New Delhi", "Sydney", "Beijing", "Rio", "London", "Lagos", "Moscow", "Cairo", "Tokyo", "Cape Town", "Bogota"];
-        for (var i=0; i < cities.length; i++) {
-        	// Use the JavaScript client library to create a search.list() API call.
-        	var request = gapi.client.youtube.search.list({
-        	    q: query.song + " dance " + cities[i],
-        	    maxResults: 5,
-        	    part: "id"
-        	}); 
-        	
-        	// Send the request to the API server,
-        	// and invoke onSearchRepsonse() with the response.
-        	request.execute(onSearchResponse);
-        }
+	var cities = ["Los Angeles", "New York", "New Delhi", "Sydney", "Beijing", "Rio", "London", "Lagos", "Moscow", "Cairo", "Tokyo", "Cape Town", "Bogota"];
+    for (var i=0; i < cities.length; i++) {
+    	// Use the JavaScript client library to create a search.list() API call.
+    	var request = gapi.client.youtube.search.list({
+    	    q: query.song + " dance " + cities[i],
+    	    maxResults: 5,
+    	    part: "id"
+    	}); 
+    	
+    	// Send the request to the API server,
+    	// and invoke onSearchRepsonse() with the response.
+    	request.execute(onSearchResponse);
     }
-}, 500);
+}
